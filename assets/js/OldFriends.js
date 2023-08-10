0f52a7f8b5c1a9d49274c92de0f0fcf39fd1e03e
DiceBreakers <141854239+DiceBreakers@users.noreply.github.com>
2023-08-10 14:39:38 -0500

    DumbCommas

--- assets/js/OldFriends.js
+++ assets/js/OldFriends.js
@@ -10,11 +10,11 @@ const OldFriendsArray = [
     "Mini Playlist - Queue up one song and share a reason you like it.",
     "Affirmations - Say something you admire about another person.",
     "Share a time you were embarrassed, or has become embarrassing with time.",
-    "Share any happy thought or memory."
-    "Share a goal you'd like to accomplish at some point in your life."
-    "Self-Affirmation - Share one character trait about yourself that you're proud of."
-    "Share something you want to learn or improve at."
-    "Share something that made you laugh recently."
+    "Share any happy thought or memory.",
+    "Share a goal you'd like to accomplish at some point in your life.",
+    "Self-Affirmation - Share one character trait about yourself that you're proud of.",
+    "Share something you want to learn or improve at.",
+    "Share something that made you laugh recently.",
 ]
 
 var randomQ = OldFriendsArray[(Math.floor(Math.random() * (OldFriendsArray.length)))];
